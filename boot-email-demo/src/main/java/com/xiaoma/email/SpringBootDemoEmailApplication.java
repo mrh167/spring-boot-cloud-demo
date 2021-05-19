c3c9551a3cad24161d83af89526df88f56b1659f
marryLoveJava <mrh100353@163.com>
2021-05-20 01:44:42 +0800

    boot整合email邮件

---
+++ boot-email-demo/src/main/java/com/xiaoma/email/SpringBootDemoEmailApplication.java
@@ -0,0 +1,12 @@
+package com.xiaoma.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoEmailApplication.class, args);
+    }
+}
